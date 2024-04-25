6a16e620902e8c716f3282366679553c5a344d8e
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-04-25 22:57:48 +0200

    header still need work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,18 @@ import Link from "next/link";
 export default function Header() {
   return (
     <>
-      <nav>
-        <ul className="flex pl-6 gap-6 medium-size">
+      <nav className="flex pl-5 pt-1">
+        <div className="pt-4">
+          <Link href={"/"}>
+            <img
+              src="images/diversa-logo.svg"
+              alt="Diversa Logo"
+              className="h-8 w-auto"
+            />
+          </Link>
+        </div>
+
+        <ul className="flex pl-10 gap-6 medium-size">
           <li>
             <Link href={"/"} prefetch={false}>
               Hjem

f9a1b0d3a8053c0d3ee53435be40ddc85de6de8b
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-04-25 10:42:22 +0200

    ok

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   return (
     <div>
-      <h1>Welcome to our Accessibility Report Tool</h1>
+      <h1 className={baskerville.className}>Welcome to our Accessibility Report Tool</h1>
       <form action="/report">
         <input type="text" id="url" name="url" placeholder="Enter a URL" required />
         <button type="submit">Get Report</button>

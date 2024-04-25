da61c21eac7f0bbcb831bf670ac6a2abbd54e06a
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-04-25 10:39:26 +0200

    added src pages

---
+++ src/app/report/error.jsx
@@ -0,0 +1,9 @@
+"use client"
+export default function ReportError() {
+  return (
+    <div>
+      <h1>Error Generating Report</h1>
+      <p>There was an error generating the report. Please try again.</p>
+    </div>
+  );
+}
---
+++ src/app/report/page.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+export const revalidate = 1800;
+
+export default async function Page({ searchParams }) {
+    const params = new URLSearchParams(searchParams);
+    const response = await fetch(
+        `https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`,
+    );
+    const data = await response.json();
+
+    return (
+        <main>
+            <h1>Report for {data.url}</h1>
+            <p>Found {data.violations.length} issues</p>
+
+            <div>
+                <p>Number of issues found: {data.violations.length}</p>
+                <p>Din score er {100 - data.violations.length / 53 * 100}%</p>
+                {data.violations.map((violation, index) => (
+                    <div key={index}>
+                        <h2>{violation.description}</h2>
+                        <p>Impact: {violation.impact}</p>
+                    </div>
+                ))}
+            </div>
+
+            <Image
+                alt={data.url}
+                src={data.screenshot.url}
+                width={data.screenshot.width}
+                height={data.screenshot.height}
+            />
+        </main>
+    );
+}

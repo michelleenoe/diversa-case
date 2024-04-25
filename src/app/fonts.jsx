e0fbabcf0ad2672360cee80e266dec5a88025132
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-04-25 09:57:38 +0200

    costum properties, fonts, svg

---
+++ src/app/fonts.jsx
@@ -0,0 +1,12 @@
+import { Libre_Baskerville, Poppins } from "next/font/google";
+
+export const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+export const baskerville = Libre_Baskerville({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});

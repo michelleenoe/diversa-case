19e84cbc5dbcb9884cb1c0a7df70e7eb137c6d64
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-04-26 09:35:29 +0200

    button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,33 +4,37 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center leading-5 ">
-      <div className="large-size leading-5 pb-5">
-        <h1 className={baskerville.className}>
-          Welcome to our Accessibility Report Tool
-        </h1>
-      </div>
-      <p className="leading-6 pb-10 small-size">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
-        voluptatum facere eaque voluptates aliquid amet ex architecto! Aperiam
-        quas, assumenda dolorem quibusdam, quisquam ducimus quia, culpa non
-        dolor eos exercitationem!
-      </p>
-      <form className="flex items-center gap-6" action="/report">
-        <div className="flex flex-col">
-          <label htmlFor="url" className="small-size text-gray-700">
-            Enter Url
-          </label>
-          <input
-            className=" bg-inputfieldcolor rounded-full px-3 py-2"
-            type="text"
-            id="url"
-            name="url"
-            required
-          />
+    <div className="custom-grid pt-10">
+      <div className="flex flex-col items-center">
+        <div className="large-size pb-5 max-w-2xl">
+          <h1 className={baskerville.className}>
+            Velkommen til vores Værktøj til Tilgængelighedsrapportering
+          </h1>
         </div>
-        <Button title="Få report" backgroundColor="" />
-      </form>
+        <p className="pb-8 small-size max-w-2xl">
+          Vores Tilgængelighedsrapportværktøj viser forbedringsområder og
+          overholdelse af standarder for dit webindhold. Sikre tilgængelighed
+          for et bredt publikum og en inkluderende onlineoplevelse. Start
+          forbedringen i dag!
+        </p>
+        <form className="flex flex-col items-center gap-6" action="/report">
+          <div className="flex flex-col ">
+            <label htmlFor="url" className="small-size pb-2">
+              Indtast venligst URL
+            </label>
+            <div className="flex flex-wrap gap-3">
+              <input
+                className="bg-inputfieldcolor rounded-full px-3 py-2 mr-3"
+                type="text"
+                id="url"
+                name="url"
+                required
+              />
+              <Button title="Få Report" backgroundColor="" />
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }

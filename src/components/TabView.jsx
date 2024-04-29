a2087b478364f24e74745e5d03cdebfb28855311
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-04-29 10:28:59 +0200

    data greater than zero

--- src/components/TabView.jsx
+++ src/components/TabView.jsx
@@ -28,15 +28,20 @@ export default function TabView({ data }) {
       }
     };
 
-    // Call it on mount and set up an event listener for resizing
+
     updateArrows();
     window.addEventListener('resize', updateArrows);
 
-    // Clean up the event listener on unmount
+
     return () => {
       window.removeEventListener('resize', updateArrows);
     };
   }, []);
+
+
+  const filteredCategories = Object.keys(categories).filter(
+    category => categories[category].length > 0
+  );
 
   return (
     <div className="px-2 py-16 sm:px-0 m-4 relative">
@@ -48,7 +53,7 @@ export default function TabView({ data }) {
                 &lsaquo;
               </span>
             )}
-            {Object.keys(categories).map((category) => (
+            {filteredCategories.map((category) => (
               <Tab key={category} className={({ selected }) =>
                 classNames(
                   "rounded-t-2xl py-3 px-10 small-size",
@@ -71,48 +76,50 @@ export default function TabView({ data }) {
           </Tab.List>
         </div>
         <Tab.Panels className="bg-tabbgcolor col-span-4">
-          {Object.values(categories).map((posts, idx) => (
-            <Tab.Panel
-              key={idx}
-              className={classNames(
-                "rounded-xl bg-white p-3",
-                "ring-white ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
-              )}
-            >
-              <section>
-                <div className={baskerville.className}>
-                  <p className="normal-size text-center pt-7 pb-6">
-                    These results indicate what elements failed the axe-core rules
-                  </p>
-                </div>
-                <ul className="px-4 md:px-16">
-                  {posts.map((post) => (
-                    <li
-                      key={post.id}
-                      className="flex flex-col md:flex-row justify-between items-center py-8 border-b border-y-tabbordercolor"
-                    >
-                      <div className="flex-shrink-0 md:mr-4">
-                        <div className="p-2 w-4 h-4 aspect-square rounded-full bg-cColor"></div>
-                      </div>
-                      <div className="flex-grow md:max-w-md">
-                        <h2 className="small-size">{post.description}</h2>
-                        <h3 className="small-size">{post.help}</h3>
-                      </div>
-                      <div>
-                        <Link
-                          className="bg-primarycolor02 hover:bg-primarycolorvariant03 px-5 py-3 rounded-full"
-                          href={`/rules/${post.id}`}
-                          prefetch={false}
-                        >
-                          Læs mere
-                        </Link>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </section>
-            </Tab.Panel>
-          ))}
+          {Object.values(categories)
+            .filter(posts => posts.length > 0)
+            .map((posts, idx) => (
+              <Tab.Panel
+                key={idx}
+                className={classNames(
+                  "rounded-xl bg-white p-3",
+                  "ring-white ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
+                )}
+              >
+                <section>
+                  <div className={baskerville.className}>
+                    <p className="normal-size text-center pt-7 pb-6">
+                      These results indicate what elements failed the axe-core rules
+                    </p>
+                  </div>
+                  <ul className="px-4 md:px-16">
+                    {posts.map((post) => (
+                      <li
+                        key={post.id}
+                        className="flex flex-col md:flex-row justify-between items-center py-8 border-b border-y-tabbordercolor"
+                      >
+                        <div className="flex-shrink-0 md:mr-4">
+                          <div className="p-2 w-4 h-4 aspect-square rounded-full bg-cColor"></div>
+                        </div>
+                        <div className="flex-grow md:max-w-md">
+                          <h2 className="small-size">{post.description}</h2>
+                          <h3 className="small-size">{post.help}</h3>
+                        </div>
+                        <div>
+                          <Link
+                            className="bg-primarycolor02 hover:bg-primarycolorvariant03 px-5 py-3 rounded-full"
+                            href={`/rules/${post.id}`}
+                            prefetch={false}
+                          >
+                            Læs mere
+                          </Link>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </section>
+              </Tab.Panel>
+            ))}
         </Tab.Panels>
       </Tab.Group>
     </div>

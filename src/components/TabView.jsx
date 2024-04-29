d6020e6d00c30a1a6495c4d76af13bdceb6f62b3
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-04-29 12:20:41 +0200

    tabs

--- src/components/TabView.jsx
+++ src/components/TabView.jsx
@@ -40,7 +40,7 @@ export default function TabView({ data }) {
     <div className="px-2 py-16 sm:px-0 ">
       <Tab.Group>
         <Tab.List
-          className="flex space-x-1 rounded-xl bg-blue-900/20 p-1"
+          className="flex space-x-1 rounded-xl p-1"
           ref={tabListRef}
           onKeyDown={(e) => {
             if (e.key === "ArrowRight") {
@@ -63,11 +63,11 @@ export default function TabView({ data }) {
               key={category}
               className={({ selected }) =>
                 classNames(
-                  "w-full flex justify-center items-center gap-5 rounded-lg py-2.5 text-sm font-medium leading-5",
-                  "ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                  "w-full flex justify-center items-center gap-5 rounded-lg py-2.5 text-sm font-medium leading-5 focus:outline-none  ",
+                  "focus:outline-none",
                   selected
-                    ? "bg-white text-blue-700 shadow"
-                    : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
+                    ? "bg-tabbgcolor text-secondarycolor-700 shadow"
+                    : "bg-tabtopnotactive text-secondarycolor-100 hover:bg-primarycolor02 hover:bg-white/[0.12] hover:text-white"
                 )
               }
             >
@@ -81,13 +81,13 @@ export default function TabView({ data }) {
             </Tab>
           ))}
         </Tab.List>
-        <Tab.Panels className="mt-2">
+        <Tab.Panels className="bg-tabbgcolor mt-2">
           {Object.values(categories).map((posts, idx) => (
             <Tab.Panel
               key={idx}
               className={classNames(
                 "rounded-xl bg-white p-3",
-                "ring-secondarycolor ring-offset-2 ring-offset-secondarycolor focus:outline-none focus:ring-2"
+                "ring-white ring-offset-2 ring-offset-secondarycolor focus:outline-none focus:ring-2"
               )}
             >
               <section>

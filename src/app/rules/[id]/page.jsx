f76f2c8166c6613485b303e2daa99c18557c6787
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-04-29 09:05:34 +0200

    ok

--- src/app/rules/[id]/page.jsx
+++ src/app/rules/[id]/page.jsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import rulesData from '../rulesData.json';
-import { baskerville } from "../../fonts";
-=======
 import rulesData from "../rulesData.json";
 import { baskerville } from "@/app/fonts";
->>>>>>> tailwind
 
 export default function RuleDetail({ params }) {
   const { id } = params;
@@ -12,30 +7,6 @@ export default function RuleDetail({ params }) {
     rulesData.find((rule) => rule.id === id) ||
     rulesData.find((rule) => rule.id === "Lorem ipsum");
 
-<<<<<<< HEAD
-    return (
-        <main>
-            <div className="custom-grid">
-                <section className="max-w-[var(--content-width)] mx-auto  shadow-lg overflow-hidden">
-                    <div className="p-6 space-y-4 large-size max-w-2xl">
-                        <h1 className={baskerville.className}>{rule.header}</h1>
-                        <p className="small-size">{rule.descriptionlong}</p>
-                        <div className="space-y-2">
-                            <p className="normal-size">
-                                Sådan fikser du det:<br />
-                                <span className="small-size">{rule.howtofix}</span>
-                            </p>
-                            <p className="normal-size">
-                                Hvilken betydning har problemet for tilgængeligheden?<br />
-                                <span className="small-size">{rule.whyitmatters}</span>
-                            </p>
-                            <p className="small-size">
-                                Læs mere her: <a className="link" href={rule.url} target="_blank" rel="noopener noreferrer">{rule.url}</a>
-                            </p>
-                        </div>
-                    </div>
-                </section>
-=======
   return (
     <>
       <div className="custom-grid max-w-3xl">
@@ -43,7 +14,6 @@ export default function RuleDetail({ params }) {
           <div className="p-6 space-y-4">
             <div className={baskerville.className}>
               <h1 className="large-size max-w-2xl">{rule.header}</h1>
->>>>>>> tailwind
             </div>
             <p className="small-size pb-4">{rule.descriptionlong}</p>
             <div>

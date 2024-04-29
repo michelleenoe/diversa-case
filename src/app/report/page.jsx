1f14af54e37950e7b6afd182ac750ab7524490ca
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-04-29 21:27:52 +0200

    tab page

--- src/app/report/page.jsx
+++ src/app/report/page.jsx
@@ -10,7 +10,8 @@ const SCORE_COLORS = {
 };
 
 const beregnScore = (data) => {
-  const antalProblemer = data.violations.length + data.inapplicable.length + data.incomplete.length;
+  const antalProblemer =
+    data.violations.length + data.inapplicable.length + data.incomplete.length;
   return Math.round(100 - (antalProblemer / 58) * 100);
   {
     /*https://dequeuniversity.com/rules/axe/html/4.9 der er 58 på hjemmesiden*/
@@ -25,7 +26,9 @@ const SamletScoreBogstav = (score) => {
 
 export default async function Side({ searchParams }) {
   const params = new URLSearchParams(searchParams);
-  const response = await fetch(`https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`);
+  const response = await fetch(
+    `https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`
+  );
   const data = await response.json();
 
   const score = beregnScore(data);
@@ -33,26 +36,64 @@ export default async function Side({ searchParams }) {
 
   return (
     <>
-      <section className="grid grid-cols-1 sm:grid-cols-2 pt-10">
-        <article className="grid justify-center">
-          <div className="medium-size">
+      <div className="flex justify-center px-2 py-16 sm:px-0">
+        <div className="max-w-6xl mx-auto w-full">
+          <div className="medium-size text-center pb-0 md:pb-8">
             <h1 className={baskerville.className}>Rapport for {data.url}</h1>
           </div>
-          <div className="grid justify-items-center">
-            <h2 className="medium-size">Din score er {score}%</h2>
-            <div className="aspect-square flex items-center justify-center px-5 py-1 rounded-full" style={{ backgroundColor: color }}>
-              <p className="text-2xl font-bold">{letter}</p>
+          <div className="card bg-white shadow-lg rounded-lg overflow-hidden">
+            <div className="p-5">
+              <section className="grid grid-cols-1 sm:grid-cols-2 sm:pt-1">
+                <article className="grid justify-center">
+                  <div className="grid justify-items-center">
+                    <h2 className="medium-size">Din score er {score}%</h2>
+                    <div
+                      className="aspect-square flex items-center justify-center px-5 py-1 rounded-full"
+                      style={{ backgroundColor: color }}
+                    >
+                      <p className="text-2xl font-bold">{letter}</p>
+                    </div>
+                  </div>
+                  <p className="small-size pt-3 text-center">
+                    Antal problemer fundet:{" "}
+                    {data.violations.length +
+                      data.inapplicable.length +
+                      data.incomplete.length}
+                  </p>
+                  <div className="grid grid-cols-3 items-center py-3 gap-4">
+                    <div className="flex flex-col items-center justify-center gap-2 ">
+                      <span className=" w-5 h-5 aspect-square  rounded-full bg-aColor"></span>
+                      <span className=" text-sm small-size">Fungerende</span>
+                    </div>
+                    <div className="flex flex-col items-center gap-2 ">
+                      <span className=" w-5 h-5 aspect-square rounded-full bg-bColor"></span>
+                      <span className=" text-sm small-size">Advarsel</span>
+                    </div>
+                    <div className="flex flex-col items-center gap-2 ">
+                      <span className=" w-5 h-5 aspect-square rounded-full bg-cColor"></span>
+                      <span className=" text-sm small-size">Fejl</span>
+                    </div>
+                  </div>
+                </article>
+
+                <figure className="grid justify-items-center p-8">
+                  <Image
+                    className="w-3/4"
+                    alt={data.url}
+                    src={data.screenshot.url}
+                    width={data.screenshot.width}
+                    height={data.screenshot.height}
+                  />
+                </figure>
+              </section>
             </div>
           </div>
-          <p className="small-size pt-3 text-center">Antal problemer fundet: {data.violations.length + data.inapplicable.length + data.incomplete.length}</p>
-        </article>
-        <figure className="grid justify-items-center p-8">
-          <Image className="w-3/4" alt={data.url} src={data.screenshot.url} width={data.screenshot.width} height={data.screenshot.height} />
-        </figure>
-      </section>
-      <section className="">
-        <TabView data={data} />
-      </section>
+
+          <section className="">
+            <TabView data={data} />
+          </section>
+        </div>
+      </div>
     </>
   );
 }

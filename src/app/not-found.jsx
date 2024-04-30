77b2944d42d62eb4ff8d543de3843f52c9abe3b6
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2024-04-30 20:13:41 +0200

    not found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,10 +2,16 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <section className="grid grid-cols-gridContent">
+      <div className="pt-10 mb-20 col-start-3">
+        <div className="large-size pb-6 max-w-2xl">
+          <h1 className={baskerville.className}>Siden findes ikke</h1>
+        </div>
+        <p className="pb-8 small-size max-w-2xl">Siden du prøver at finde, findes desværre ikke. </p>
+        <Link className="bg-primarycolor02 hover:bg-primarycolorvariant03 px-5 py-3 rounded-full" href={"/"} prefetch={false}>
+          Tilbage til forsiden
+        </Link>
+      </div>
+    </section>
   );
 }

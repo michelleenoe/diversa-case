264dd6354ce1c8739806406fa7352dd228d02b41
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-05-01 20:53:07 +0200

    img

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,8 @@ import React from "react";
 
 export const metadata = {
   title: "tilgængelighedsrapportering",
-  description: "Tilgængelighedsrapporteringsværktøj - Opgave med Charlie Tango for Diversa. Multimediedesign Frontend 2024",
+  description:
+    "Tilgængelighedsrapporteringsværktøj - Opgave med Charlie Tango for Diversa. Multimediedesign Frontend 2024",
 };
 
 export default function Home() {
@@ -13,16 +14,30 @@ export default function Home() {
       <div className="py-16 col-start-3">
         <div>
           <div className="large-size pb-6 max-w-2xl">
-            <h1 className={baskerville.className}>Velkommen til vores værktøj til tilgængelighedsrapportering</h1>
+            <h1 className={baskerville.className}>
+              Velkommen til vores værktøj til tilgængelighedsrapportering
+            </h1>
           </div>
-          <p className="pb-8 small-size max-w-2xl">Vores tilgængelighedsrapportværktøj viser forbedringsområder og overholdelse af standarder for dit webindhold. Sikre tilgængelighed for et bredt publikum og en inkluderende onlineoplevelse. Start forbedringen i dag!</p>
+          <p className="pb-8 small-size max-w-2xl">
+            Vores tilgængelighedsrapportværktøj viser forbedringsområder og
+            overholdelse af standarder for dit webindhold. Sikre tilgængelighed
+            for et bredt publikum og en inkluderende onlineoplevelse. Start
+            forbedringen i dag!
+          </p>
           <form action="/report">
             <div className="flex flex-col ">
               <label htmlFor="url" className="small-size pb-2">
                 Indtast venligst URL
               </label>
               <div className="flex flex-wrap gap-3">
-                <input className="bg-inputfieldcolor rounded-full bordercolor  px-3 py-2 mr-3 w-2/3 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondarycolor" type="text" id="url" name="url" required />
+                <input
+                  className="bg-inputfieldcolor rounded-full bordercolor  px-3 py-2 mr-3 w-2/3 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondarycolor"
+                  type="text"
+                  id="url"
+                  name="url"
+                  required
+                />
+
                 <Button className="w-1/3" title="Få Report" type="submit" />
               </div>
             </div>

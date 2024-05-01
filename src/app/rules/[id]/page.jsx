f04b70804df03ef2ef59ce96d4d166055c6436ae
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2024-05-01 10:58:24 +0200

    py add

--- src/app/rules/[id]/page.jsx
+++ src/app/rules/[id]/page.jsx
@@ -4,9 +4,7 @@ import Link from "next/link";
 
 export async function generateMetadata({ params }) {
   const { id } = params;
-  const data =
-    rulesData.find((rule) => rule.id === id) ||
-    rulesData.find((rule) => rule.id === "Lorem ipsum");
+  const data = rulesData.find((rule) => rule.id === id) || rulesData.find((rule) => rule.id === "Lorem ipsum");
 
   return {
     title: data.id,
@@ -21,16 +19,14 @@ export async function generateMetadata({ params }) {
 export default function RuleDetail({ params }) {
   const { id } = params;
 
-  const rule =
-    rulesData.find((rule) => rule.id === id) ||
-    rulesData.find((rule) => rule.id === "Lorem ipsum");
+  const rule = rulesData.find((rule) => rule.id === id) || rulesData.find((rule) => rule.id === "Lorem ipsum");
 
   return (
     <>
       <div className="grid grid-cols-gridContent">
-        <section className="col-start-3">
+        <section className="col-start-3 py-16">
           <div className={baskerville.className}>
-            <h1 className=" pt-10 pb-5 large-size max-w-2xl">{rule.header}</h1>
+            <h1 className="pb-5 large-size max-w-2xl">{rule.header}</h1>
           </div>
           <div className="card bg-white shadow-lg rounded-lg overflow-hidden">
             <div className="p-6 space-y-4">
@@ -44,16 +40,10 @@ export default function RuleDetail({ params }) {
                 <p className="normal-size pt-4">
                   Hvordan påvirker problemet <br /> tilgængeligheden?
                   <br />
-                  <p className="small-size max-w-xl pt-1">
-                    {rule.whyitmatters}
-                  </p>
+                  <p className="small-size max-w-xl pt-1">{rule.whyitmatters}</p>
                 </p>
                 <p className="flex justify-end small-size pt-3">
-                  <Link
-                    className="bg-primarycolor01  hover:bg-primarycolorvariant01 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondarycolor bordercolor px-5 py-3 rounded-full"
-                    href={`/${rule.id}`}
-                    prefetch={false}
-                  >
+                  <Link className="bg-primarycolor01  hover:bg-primarycolorvariant01 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondarycolor bordercolor px-5 py-3 rounded-full" href={`/${rule.id}`} prefetch={false}>
                     Gå til regelsæt
                   </Link>
                 </p>
